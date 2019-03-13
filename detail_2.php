73ddfb65766fad989ca0b6a7f1f996ac306e5ab4
Elneris Dang <elneris.dang@gmail.com>
2019-03-13 16:02:37 +0100

    suppression

---
+++ detail_2.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'header.php';
+
+?>
+
+            <!-- IMAGE ENTETE -->
+
+            <div class="container">
+              <div class="intro-text">
+                 <div class="intro-heading text-uppercase"><br></div>
+                 
+              </div>
+           </div>
+        </header>
+
+        <!-- CONTENU -->
+        
+        <div class="container">
+            <div class="row detail_row">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 detail_div"><img class="detail_img" src="image/julie3.jpg" alt="Julie"/> </div> 
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 detail_descri detail_div lang-JS">
+                    <p>
+                        <span class="police-gras"> Nom :</span> Charlemagne
+                        <br><span class="police-gras"> Prénom :</span> Julie
+                        <br><span class="police-gras"> Age :</span> Age : 24 ans
+                        <br><span class="police-gras"> Email :</span> <br> julie.charlemagne@live.fr
+                        <br><span class="police-gras"> Linkedin :</span> Pas encore.
+                        <br><span class="police-gras"> Language étudié :</span>  JS
+                    </p>
+                </div> 
+            </div>
+            <div class="row detail_present detail_row">       
+                <h2><span class="detail_titre">Présentation</span></h2>
+                <br>
+                <p>Julie, 24 ans, j'ai grandi a Pau, depuis le lycée je cotoie la région de Bordeaux, étant sur le bassin d'arcachon. Je suis venue sur Bordeaux après mon bac il y a 5 ans, et j'étais à la recherche de ma voie profetionelle jusqu'a octobre 2018.</p>
+                
+                <p>J'ai fais un bac S spé Math en <b>2013</b>. J'ai voulu continuer mes études dans la voie scientifque mais je me suis perdue dans des options qui ne me convenait pas. 
+                 <br> Vient ensuite une recherche de profession qui me plairait, en cherchant dans mes hobits, et ce fu désilusion en désilusion, et c'est suite à une remise en question et un bilan de compétences qu'il a été mis en évidence que mon profil convenait au developpement informatique.
+                 <br>Suite à celà je me suis mise à coder toute seule et à suivre des tutos, il s'est averé que je m'y suis beaucoup plu.
+                 <br>J'ai alors cherché une formation diplomante, et me voilà à la Wild Code School.
+                 <br>Je suis contente d'être là aujourd'hui et j'espère que mon avenir sera plus clair de que passé.
+                </p>
+            </div>
+        </div>
+
+<?php
+
+require 'footer.php';
+
+?>
---
+++ detail_3.php
@@ -0,0 +1,47 @@
+<?php
+
+require 'header.php';
+
+?>
+
+            <!-- IMAGE ENTETE -->
+
+            <div class="container">
+              <div class="intro-text">
+                 <div class="intro-heading text-uppercase"><br></div>
+                 
+              </div>
+           </div>
+        </header>
+
+        <!-- CONTENU -->
+        
+        <div class="container">
+            <div class="row detail_row">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 detail_div"><img class="detail_img" src="image/nico3.jpg" alt="Nicolas"/> </div> 
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 detail_descri detail_div lang-JS">
+                    <p>
+                        <span class="police-gras"> Nom :</span> Hosteins
+                        <br><span class="police-gras"> Prénom :</span> Nicolas
+                        <br><span class="police-gras"> Age :</span> 41 ans
+                        <br><span class="police-gras"> Email :</span>
+                        <br> n.hosteins@gmail.com
+                        <br><span class="police-gras"> Linkedin :</span><!-- <a href="https://www.linkedin.com/in/elneris-dang-283b56174/">-->un jour, promis
+                        <br><span class="police-gras"> Langage étudié :</span> JS
+                    </p>
+                </div> 
+            </div>
+            <div class="row detail_present detail_row">       
+                <h2><span class="detail_titre">Présentation</span></h2>
+                <br>
+                                
+                <p>Après un diplome d'environnement en 2002, j'ai travaillé un an dans ce domaine. Pendant la période de chomage suivante, je me suis inscrit en bénévole dans une salle de concert. J'ai travaillé ainsi 15 ans dans ce domaine des musiques amplifiées, sans le chercher vraiment, mais avec passion.  </p>
+                <p>Aujourd'hui, la passion s'étant érodée, je me reconverti dans le développement web, métier plus traditionnel mais certainement tout aussi interessant. </p>        
+            </div>
+        </div>
+
+<?php
+
+require 'footer.php';
+
+?>
