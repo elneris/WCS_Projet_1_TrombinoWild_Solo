bdbb48ac4abda0ae485ec87bd617071deb50280a
Elneris Dang <elneris.dang@gmail.com>
2019-03-20 14:48:31 +0100

    ajout de la bdd

--- fonction.php
+++ fonction.php
@@ -1,25 +1,26 @@
 <?php
 
 
-function wildersCard($parametre){
+function wildersCard($parametre)
+{
 
-    require 'wilders.php';
-
+    require 'connection.php'; ?>
+    <?php
     foreach ($wilders as $wilder => $value) {
-        if ($value[3] == $parametre) {?>
+        if ($value['cursus'][0] == $parametre) {?>
             <div class="col-12 col-md-6 col-lg-3 trombi_carte">
                 <div class="card ">
-                    <a href="detail.php?wilder=<?php echo strtolower($wilder); ?>">
-                    <div class="card-img-top">
-                            <img class="card-img-top" src="image/photo/<?php echo strtolower($name[$wilder][1]); ?>.jpg">
+                    <a href="detail.php?wilder=<?php echo ($value['id'][0]); ?>">
+                        <div class="card-img-top">
+                            <img class="card-img-top" src="image/photo/<?php echo strtolower($value['id'][0]); ?>.jpg">
                         </div>
                         <div class="card-body carte_body">
-                            <h5 class="card-title trombi_nom"><?php echo $wildersName[$wilder] ; ?></h5>
+                            <h5 class="card-title trombi_nom"><?php echo ($value['firstname'][0]) . ' ' . ($value['lastname'][0]); ?></h5>
                         </div>
                     </a>
                 </div>
             </div>
-    <?php
+            <?php
         }
     }
 }
@@ -28,20 +29,20 @@ function wildersCard($parametre){
 <?php
 function infoWilders($parametre){
 
-    require 'wilders.php';
+    require 'connection.php';
 
     foreach ($wilders as $wilder => $value){
-        if ($wilder == $parametre){?>
-        <div class="col-lg-6 col-12 detail_div"><img class="detail_img" src="image/photo/<?php echo strtolower($name[$wilder][1]); ?>.jpg" alt="Elneris"/> </div>
+        if ($value['id'][0] == $parametre){?>
+        <div class="col-lg-6 col-12 detail_div"><img class="detail_img" src="image/photo/<?php echo ($value['id'][0]); ?>.jpg" alt=""/> </div>
                 <div class="col-lg-6 col-12 detail_descri detail_div ">
                     <p>
-                        <span class="police-gras"> Nom :</span> <?php echo ($name[$wilder][1]); ?>
-                        <br> <span class="police-gras"> Prénom :</span> <?php echo ($name[$wilder][0]); ?>
-                        <br> <span class="police-gras"> Age :</span> <?php echo ($wildersAge[$wilder]); ?>
+                        <span class="police-gras"> Nom :</span> <?php echo ($value['lastname'][0]); ?>
+                        <br> <span class="police-gras"> Prénom :</span> <?php echo ($value['firstname'][0]); ?>
+                        <br> <span class="police-gras"> Age :</span> <?php echo birthday(($value['birthday'][0])); ?>
         <br> <span class="police-gras"> Email :</span>
-                        <br> <?php echo ($wildersMail[$wilder]); ?>
-        <br> <span class="police-gras"> Github :</span> <a href="https://github.com/<?php echo ($gitHub[$wilder]); ?>" target="_blank"><?php echo $gitHub[$wilder]; ?></a>
-                        <br> <span class="police-gras"> Langage étudié :</span> <?php echo ($wildersLanguage[$wilder]) ;?>
+                        <br> <?php echo ($value['email'][0]); ?>
+        <br> <span class="police-gras"> Github :</span> <a href="https://github.com/<?php echo ($value['github'][0]); ?>" target="_blank"><?php echo ($value['github'][0]); ?></a>
+                        <br> <span class="police-gras"> Langage étudié :</span> <?php echo ($value['cursus'][0]); ?>
                     </p>
                 </div>
         <?php
@@ -52,11 +53,20 @@ function infoWilders($parametre){
 
 <?php
 function presentationWilders($parametre){
-    require 'wilders.php';
-    foreach ($wildersPresentation as $wilder => $values){
-        if ($wilder == $parametre) {
-            $wildersLastPresentation= array_pop($values);
-            echo implode(',',$values) . $wildersLastPresentation . '.';
+    require 'connection.php';
+    foreach ($wilders as $wilder => $value){
+        if ($value['id'][0] == $parametre) {
+            echo ($value['presentation'][0]);
         }
     }
 }
+?>
+
+<?php
+
+function birthday($birthday) {
+    $age = date_create($birthday)->diff(date_create('today'))->y;
+
+    return $age;
+}
+echo birthday('1989-11-05');
--- test.php
+++ test.php
@@ -1,88 +1,37 @@
 <?php
 
-function wildersCard($language)
+function getWilders()
 {
-	if ($language == 'PHP')
-	{
-		foreach ($wilders as $wilder => $value) 
-		{ ?>
-			<div class="col-12 col-md-6 col-lg-3 trombi_carte">   		
-				<div class="card " style="width: 18rem;">
-					<a href="detail.php">
-						<div class="card-img-top">
-							<img class="card-img-top" src="image/photo/<?php echo strtolower($name[$wilder][1]); ?>.jpg">
-						</div>
-						<div class="card-body">
-				 			<h5 class="card-title trombi_nom"><?php echo $wildersName[$wilder] ; ?></h5>
-				 		</div>
-				   	</a>		           	
-			   	</div>
-			</div>
-			<?php 
-		}
-	} 
+    $bdd = mysqli_connect('localhost', 'root', 'root', 'trombino')
+    or die('could not connect to database');
+    $query = 'SELECT * FROM wilder';
+    $response = $bdd->query($query);
+    $wilders = array();
+    while ($data = mysqli_fetch_array($response))
+    {
+        $wildersId = $data['id'];
+        $wildersFirstname = $data['firstname'];
+        $wildersLastname = $data['lastname'];
+        $wildersBirthday = $data['birthday'];
+        $wildersEmail = $data['email'];
+        $wildersCursus = $data['cursus'];
+        $wildersGithub = $data['github'];
+        $wildersPresentation = $data['presentation'];
+
+
+        $wilders[] = [
+            'id' => [$wildersId],
+            'firstname' =>[$wildersFirstname],
+            'lastname' =>[$wildersLastname],
+            'birthday' =>[$wildersBirthday],
+            'email' =>[$wildersEmail],
+            'cursus' =>[$wildersCursus],
+            'github' =>[$wildersGithub],
+            'presentation' =>[$wildersPresentation],
+            ];
+    }
+    return $wilders;
 }
+
+var_dump(getWilders('PHP'));
 ?>
-<?php
-
-require 'header.php';
-require 'wilders.php';
-?>
-		</header>
-
-<div class="container">
-	<div class = "pillules">
-		<ul class="nav nav-pills mb-3 trombi_onglet" id="pills-tab" role="tablist">
-		  <li class="nav-item">
-		  	<form action="test.php">
-		    <input type="submit" name="tous" value="Tous" class="nav-link text-danger button active " id="pills-tous-tab" data-toggle="pill" role="tab" aria-controls="pills-tous" aria-selected="true">
-		</form>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link text-danger " id="pills-php-tab" data-toggle="pill" href="#pills-php" role="tab" aria-controls="pills-php" aria-selected="false">Php</a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link  text-danger" id="pills-js-tab" data-toggle="pill" href="#pills-js" role="tab" aria-controls="pills-js" aria-selected="false">JS</a>
-		  </li>
-		</ul>
-	</div>
-</div>
-
-<!-- TROMBI -->
-
-<div class="tab-content" id="pills-tabContent">
-   <div class="tab-pane fade show active" id="pills-tous" role="tabpanel" aria-labelledby="pills-tous-tab">
-	   	<div class="container alignement">
-			
-		</div>
-	</div>
-	<div class="tab-pane fade" id="pills-php" role="tabpanel" aria-labelledby="pills-php-tab">
-    	<div class="container alignement">
-								
-				
-			
-		</div>
-	</div>
-	<div class="tab-pane fade" id="pills-js" role="tabpanel" aria-labelledby="pills-js-tab">
-    	<div class="container alignement">					
-			
-		</div>
-	</div>
-</div>
-
-		<script>
-			$(document).ready(function(){
-			    $('.button').click(function(){
-			        var clickBtnValue = $(this).val();
-			        var ajaxurl = 'ajax.php',
-			        data =  {'action': clickBtnValue};
-			        $.post(ajaxurl, data, function (response) {
-			            // Response div goes here.
-			            alert("action performed successfully");
-			        });
-			    });
-
-			});
-		</script>
-
-<?php include 'footer.php'; ?>
